e29b2d43765fd43cc05af024ec3b23506aafd336
lc6a <1952511149@qq.com>
2021-03-25 19:46:37 +0800

    权限常量位置

--- src/main/kotlin/com/lc/spring_mirai/demo/controller/ShutdownController.kt
+++ src/main/kotlin/com/lc/spring_mirai/demo/controller/ShutdownController.kt
@@ -3,6 +3,7 @@ package com.lc.spring_mirai.demo.controller
 import com.lc.spring_mirai.annotation.EventFilter
 import com.lc.spring_mirai.annotation.PermissionFilter
 import com.lc.spring_mirai.annotation.RequestMapped
+import com.lc.spring_mirai.demo.controller.ShutdownController.Companion.SHUTDOWN_PERMISSION
 import com.lc.spring_mirai.util.ReplyUtil
 import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.data.PlainText
@@ -13,12 +14,16 @@ import javax.annotation.Resource
  * @Author 19525
  * @Date 2021/2/14 23:24
  */
-const val SHUTDOWN_PERMISSION = "关机"
+
 @Controller
 @EventFilter(MessageEvent::class)
 @PermissionFilter(SHUTDOWN_PERMISSION)
 @RequestMapped("关机")
 class ShutdownController {
+
+    companion object{
+        const val SHUTDOWN_PERMISSION = "关机"
+    }
 
     @Resource(name = "#{springMiraiBeanNameManager.beanNameConfig.getBeanName('replyUtil')}")
     private lateinit var replyUtil: ReplyUtil
